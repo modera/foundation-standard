7c460176a61d6c47d54086d0a46ccbe372e74fbc
Sergei Vizel <sergei.vizel@modera.net>
2015-02-05 12:15:17 +0200

    [MPFE-690] Symfony 2.6 upgrade

--- web/app.php
+++ web/app.php
@@ -1,27 +1,36 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Modera\DynamicallyConfigurableAppBundle\KernelConfig;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
+// Enable APC for autoloading to improve performance.
+// You should change the ApcClassLoader first argument to a unique prefix
+// in order to prevent cache key conflicts with other applications
+// also using APC.
 /*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
+$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+$loader->unregister();
+$apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $mode = KernelConfig::read();
+if ($mode['debug']) {
+    Debug::enable();
+}
+
 $kernel = new AppKernel($mode['env'], $mode['debug']);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
